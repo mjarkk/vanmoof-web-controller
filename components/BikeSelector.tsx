0b8c98990c13c839f5328a39ecc6abdf1446e450
mjarkk <mkopenga@gmail.com>
2022-06-21 14:02:32 +0200

    Add bike selector on controls-test page

---
+++ components/BikeSelector.tsx
@@ -0,0 +1,81 @@
+import { BikeCredentials } from '../lib/bike'
+import { Button } from './Button'
+
+interface BikeSelectorProps {
+    options: Array<BikeCredentials>
+    onSelect: (option: BikeCredentials, idx: number) => void
+    title: string
+}
+
+export function BikeSelector({ options, onSelect, title }: BikeSelectorProps) {
+    return (
+        <div>
+            <h2>{title}</h2>
+            <div className='bikes'>
+                {options.map((bike, idx) =>
+                    <Button
+                        key={idx}
+                        style={{ padding: 0, margin: 10 }}
+                        onClick={() => onSelect(bike, idx)}
+                    >
+                        <div
+                            className='previewImage'
+                            style={bike.links
+                                ? { backgroundImage: `url('${bike.links.thumbnail}')` }
+                                : undefined
+                            }
+                        />
+                        <div className='detials'>
+                            <h3>{bike.name}</h3>
+                            <p>{bike.ownerName}</p>
+                            <div className='meta'>
+                                <p><span>id</span>{bike.id}</p>
+                                <p><span>mac</span>{bike.mac}</p>
+                            </div>
+                        </div>
+                    </Button>
+                )}
+            </div>
+            <style jsx>{`
+                h2 {
+                    text-align: center;
+                }
+                .bikes {
+                    display: flex;
+                    flex-wrap: wrap;
+                    align-items: center;
+                }
+                .previewImage {
+                    height: 150px;
+                    background-position: center;
+                    background-size: contain;
+                    background-repeat: no-repeat;
+                    background-color: white;
+                }
+                .detials {
+                    display: flex;
+                    align-items: flex-start;
+                    flex-direction: column;
+                    padding: 20px;
+                }
+                .detials p {
+                    margin: 0;
+                }
+                .detials h3 {
+                    margin: 1px 0;
+                }
+                .detials .meta {
+                    font-size: 0.7rem;
+                    color: var(--label-color);
+                    padding-top: 10px;
+                    display: flex;
+                    align-items: flex-start;
+                    flex-direction: column;
+                }
+                .detials .meta span::after {
+                    content: ': ';
+                }
+            `}</style>
+        </div>
+    )
+}
--- pages/controls-test.tsx
+++ pages/controls-test.tsx
@@ -1,13 +1,20 @@
 import BikeControls from '../components/Controls'
-import { Bike, SpeedLimit, PowerLevel } from '../lib/bike'
+import { Bike, SpeedLimit, PowerLevel, BikeCredentials } from '../lib/bike'
 import { ApiContext, Api } from '../lib/api'
 import { useEffect, useState } from 'react'
+import { BikeSelector } from '../components/BikeSelector'
+import type { BikeAndApiCredentials } from '../components/Login'
 
 class FakeBike {
-    id = '12345'
-    mac = '1234'
+    id: string
+    mac: string
     private speedLimit = SpeedLimit.EU
     private powerLevel = PowerLevel.Fourth
+
+    constructor(credentials: BikeCredentials) {
+        this.id = credentials.id
+        this.mac = credentials.mac
+    }
 
     async bikeFirmwareVersion() {
         return '1.1.1'
@@ -37,32 +44,69 @@ class FakeBike {
     }
 }
 
+const dummyBike: BikeCredentials = {
+    id: '000000',
+    mac: '00:00:00:00:00:00',
+    encryptionKey: '00000000000000000000000000000000',
+    userKeyId: 1,
+    name: 'Susy testing bike',
+    ownerName: 'sus',
+    modelColor: {
+        name: 'Dark',
+        primary: '#25282a',
+        secondary: '#25282a',
+    },
+}
+
 export default function ControlsTest() {
-    const fakeBike = new FakeBike as unknown as Bike
-    const [api, setApi] = useState(new Api({
-        token: 'dummy',
-        refreshToken: 'dummy',
-    }))
+    const [fakeBike, setFakeBike] = useState<any>(undefined) // new FakeBike as unknown as Bike
+    const [credentials, setCredentials] = useState<BikeAndApiCredentials>({
+        api: new Api({
+            token: 'dummy',
+            refreshToken: 'dummy',
+        }),
+        bikes: [dummyBike],
+    })
 
     useEffect(() => {
         try {
             const apiCredential = localStorage.getItem('vm-api-credentials')
-            if (!apiCredential)
-                throw 'api credentials not in local storage, if you login via the / page you should have them here also'
+            const rawBikeCredentials = localStorage.getItem('vm-bike-credentials')
+
+            if (!apiCredential || !rawBikeCredentials)
+                throw 'no cached credentials that can be used, you can login on the root page (/) page to get them'
 
             const apiCredentials = JSON.parse(apiCredential)
-            setApi(new Api(apiCredentials))
+            const parsedBikeCredentials = JSON.parse(rawBikeCredentials)
+
+            const parsedApi = new Api(apiCredentials)
+
+            setCredentials({
+                api: parsedApi,
+                bikes: [dummyBike, ...parsedBikeCredentials],
+            })
         } catch (e) {
-            console.log('unable to parse api credentials from local storage, error:', e)
+            console.log('unable to parse bike/api credentials from local storage, error:', e)
         }
     }, [])
 
     return (
         <div>
             <h1>Page for testing the bike controls</h1>
-            <ApiContext.Provider value={api}>
-                <BikeControls bike={fakeBike} disconnect={() => console.log('click disconnect')} />
-            </ApiContext.Provider>
+
+            {fakeBike
+                ? <ApiContext.Provider value={credentials.api}>
+                    <BikeControls
+                        bike={fakeBike}
+                        disconnect={() => setFakeBike(undefined)}
+                    />
+                </ApiContext.Provider>
+                : <BikeSelector
+                    options={credentials.bikes}
+                    onSelect={bike => setFakeBike(new FakeBike(bike))}
+                    title='Select a bike'
+                />
+            }
 
             <style jsx>{`
                 div {
