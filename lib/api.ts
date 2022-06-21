81c6afad1432088a3aa5b850088955a6e1c2e9b7
mjarkk <mkopenga@gmail.com>
2022-06-21 16:19:16 +0200

    Make the value of the Api context always defined

--- lib/api.ts
+++ lib/api.ts
@@ -8,8 +8,6 @@ export interface ApiCredentials {
     refreshToken: string
 }
 
-export const ApiContext = createContext<Api | undefined>(undefined)
-
 export class Api {
     /*
         TODO: Add support for the refresh token
@@ -59,3 +57,5 @@ export class Api {
     }
 
 }
+
+export const ApiContext = createContext<Api>(new Api({ token: 'dummy', refreshToken: 'dummy' }))
