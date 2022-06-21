1c3ea89e88e25cc9fc53cffaa717061e6ae7b7a3
Sander Dijkxhoorn <15117158+sanderDijkxhoorn@users.noreply.github.com>
2022-06-21 14:31:46 +0200

    New api path

--- next.config.js
+++ next.config.js
@@ -5,9 +5,13 @@ const nextConfig = {
     return {
       beforeFiles: [
         {
-          source: '/api/:path',
+          source: '/api/my_vanmoof_com/:path',
           destination: 'https://my.vanmoof.com/api/v8/:path',
-        }
+        },
+        {
+          source: '/api/api_vanmoof_com/:path',
+          destination: 'https://api.vanmoof-api.com/v8/:path',
+        },
       ]
     }
   }
