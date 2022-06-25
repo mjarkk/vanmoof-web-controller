b67f2d8098bfd41c1107f3d7e9d928569f14c71a
mjarkk <mkopenga@gmail.com>
2022-06-25 12:10:31 +0200

    add service worker

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
+const withPWA = require('next-pwa')
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextConfig = withPWA({
   reactStrictMode: true,
   async rewrites() {
     return {
@@ -22,7 +24,10 @@ const nextConfig = {
         },
       ]
     }
-  }
-}
+  },
+  pwa: {
+    dest: 'public',
+  },
+})
 
 module.exports = nextConfig
