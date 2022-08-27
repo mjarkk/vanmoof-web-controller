281a6af22424f215ce00a9b8daad7f1d051d7a93
Sander Dijkxhoorn <15117158+sanderDijkxhoorn@users.noreply.github.com>
2022-08-27 18:47:42 +0200

    Moved all the stuff inside the try's

--- app/lib/api.dart
+++ app/lib/api.dart
@@ -5,10 +5,10 @@ import 'bike/from_json.dart';
 const String _apiKey = 'fcb38d47-f14b-30cf-843b-26283f6a5819';
 
 Future<ApiClient> authenticate(String email, String password) async {
-  final basicAuth = base64.encode(utf8.encode('$email:$password'));
-
-  final Response<dynamic> resp;
   try {
+    final basicAuth = base64.encode(utf8.encode('$email:$password'));
+    final Response<dynamic> resp;
+
     final client = Dio(BaseOptions(
       headers: {
         'Api-Key': _apiKey,
@@ -18,17 +18,17 @@ Future<ApiClient> authenticate(String email, String password) async {
       receiveDataWhenStatusError: true,
     ));
     resp = await client.get('https://api.vanmoof-api.com/v8/authenticate');
+
+    return ApiClient(
+      token: resp.data['token'],
+      refreshToken: resp.data['refreshToken'],
+    );
   } catch (e) {
     if (e is DioError && e.response != null) {
       throw 'Authentication failed, error: ${e.response!.data.toString()}';
     }
     throw 'Authentication failed';
   }
-
-  return ApiClient(
-    token: resp.data['token'],
-    refreshToken: resp.data['refreshToken'],
-  );
 }
 
 class ApiClient {
@@ -41,9 +41,9 @@ class ApiClient {
   final String refreshToken;
 
   getCurrentShares(int bikeid) async {
-    final Response<dynamic> resp;
-
     try {
+      final Response<dynamic> resp;
+
       final client = Dio(BaseOptions(
         headers: {
           'Api-Key': _apiKey,
@@ -54,14 +54,14 @@ class ApiClient {
       ));
       resp = await client.get(
           'https://api.vanmoof-api.com/v8/getBikeSharingInvitationsForBike/$bikeid');
+
+      return resp.data['invitations'] as List<dynamic>;
     } catch (e) {
       if (e is DioError && e.response != null) {
         throw 'Unable to obtain shares, error: ${e.response!.data.toString()}';
       }
       throw 'Unable to obtain shares, check your connection.';
     }
-
-    return resp.data['invitations'] as List<dynamic>;
   }
 
   shareCurrentBike(int bikeid, String email, int duration) async {
@@ -96,9 +96,9 @@ class ApiClient {
   }
 
   removeShare(String guid) async {
-    final Response<dynamic> resp;
-
     try {
+      final Response<dynamic> resp;
+
       final client = Dio(BaseOptions(
         headers: {
           'Api-Key': _apiKey,
@@ -110,19 +110,20 @@ class ApiClient {
 
       resp = await client.post(
           'https://api.vanmoof-api.com/v8/revokeBikeSharingInvitation/$guid');
+
+      return resp.data as Map<String, dynamic>;
     } catch (e) {
       if (e is DioError && e.response != null) {
         throw 'Unable to remove share holder, error: ${e.response!.data.toString()}';
       }
       throw 'Failed to remove the share holder.';
     }
-
-    return resp.data as Map<String, dynamic>;
   }
 
   getBikes() async {
-    final Response<dynamic> resp;
     try {
+      final Response<dynamic> resp;
+
       final client = Dio(BaseOptions(
         headers: {
           'Api-Key': _apiKey,
@@ -133,16 +134,16 @@ class ApiClient {
       ));
       resp = await client.get(
           'https://api.vanmoof-api.com/v8/getCustomerData?includeBikeDetails');
+
+      final List<dynamic> bikes = resp.data['data']['bikeDetails'];
+      if (bikes.isEmpty) throw 'You have no bikes connected to your account';
+
+      return bikes.map((b) => bikeCredentialsFromJson(b)).toList();
     } catch (e) {
       if (e is DioError && e.response != null) {
         throw 'Unable to obtain bikes, error: ${e.response!.data.toString()}';
       }
       throw 'Unable to obtain bikes';
     }
-
-    final List<dynamic> bikes = resp.data['data']['bikeDetails'];
-    if (bikes.isEmpty) throw 'You have no bikes connected to your account';
-
-    return bikes.map((b) => bikeCredentialsFromJson(b)).toList();
   }
 }
